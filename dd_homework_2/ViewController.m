ced66f5140fb14d3a645b8408c97493a2b7c202b
Andrey <andrersv96@gmail.com>
2017-10-16 18:12:58 +0300

    Add homework

--- dd_homework_2/ViewController.m
+++ dd_homework_2/ViewController.m
@@ -10,19 +10,51 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIButton *redButton;
+@property (strong, nonatomic) IBOutlet UIButton *greenButton;
+@property (strong, nonatomic) IBOutlet UIButton *blueButton;
+@property (strong, nonatomic) IBOutlet UIButton *orangeButton;
+@property (strong, nonatomic) IBOutlet UITextView *textView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.redButton setTintColor:[UIColor redColor]];
+    [self.greenButton setTintColor:[UIColor greenColor]];
+    [self.blueButton setTintColor:[UIColor blueColor]];
+    [self.orangeButton setTintColor:[UIColor orangeColor]];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)pressOnButtonWithColor:(UIButton *)sender {
+    NSRange selectedText = [self.textView selectedRange];
+
+    UIColor *attributedColor = [sender.titleLabel textColor];
+
+    NSMutableAttributedString *resultAttributedString = [[NSMutableAttributedString alloc] initWithAttributedString:self.textView.attributedText];
+    [resultAttributedString addAttribute:NSForegroundColorAttributeName
+                                   value:attributedColor
+                                   range:selectedText];
+
+    self.textView.attributedText = resultAttributedString;
+    
+}
+- (IBAction)pressOnButtonClear:(UIButton *)sender {
+    NSMutableAttributedString *resultAttributedString = [[NSMutableAttributedString alloc] initWithAttributedString:self.textView.attributedText];
+    [resultAttributedString addAttribute:NSForegroundColorAttributeName
+                                   value:[UIColor blackColor]
+                                   range:NSMakeRange(0, self.textView.text.length)];
+    
+    self.textView.attributedText = resultAttributedString;
 }
 
 
